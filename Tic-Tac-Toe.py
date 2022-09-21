d3ae6d52deaa03bf2f1a9e2448050996fad4375a
Alitares27 <aldair2795@hotmail.com>
2022-09-21 00:27:29 -0500

    update

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,3 +1,6 @@
+player1 = input('Please write the name of the first player(X): ')
+player2 = input('Please write the name of the second player(O): ')
+print(f'We will Start with {player1}')
 axis=[[1,2,3],
       [4,5,6],
       [7,8,9]]
